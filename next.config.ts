abf9f54e26daaeb621d42f2563dd8972608b485c
devesh011 <0dev.87@gmail.com>
2025-08-19 00:48:55 +0530

    fix lol3

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,21 @@
 import type { NextConfig } from "next";
 import withPWAInit from "next-pwa";
 
+// configure PWA
 const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
   buildExcludes: [/middleware-manifest\.json$/],
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development", // disable in dev
 });
 
 const nextConfig: NextConfig = {
-  distDir: "build",
   reactStrictMode: true,
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development",
   },
 };
 
+// ✅ wrap once
 export default withPWA(nextConfig);
